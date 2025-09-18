bbc1c91cd6625d0a7ab99b8f854fffd40cf0fc87
ilman84 <zaid.2010@yahoo.com>
2025-09-18 22:07:13 +0700

    chore(env): update ENV config

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,5 +4,5 @@ export const ENV = {
     process.env.NEXT_PUBLIC_API_BASE_URL || 'https://foody-api-xi.vercel.app',
   API_TOKEN:
     process.env.NEXT_PUBLIC_API_TOKEN ||
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIwLCJpYXQiOjE3NTgwMzU3ODAsImV4cCI6MTc1ODY0MDU4MH0.txII5Pj41iGdLyygtk-Dm3WVFqc8jSCNXBADbRZtbt0',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIwLCJpYXQiOjE3NTgxOTkzNTMsImV4cCI6MTc1ODgwNDE1M30.rPdJiLQmz8OxUILa3uuGA7NWIu-52CeDAaArhqx6vJc',
 } as const;
